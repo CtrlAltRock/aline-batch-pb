c6c3bdbe828a92d5b6141955bae5fdb8d94473f4
patrick-bodin <patrick.bodin@smoothstack.com>
2022-04-26 12:21:59 -0400

    NRVNA-9 implemented tests for card generation

--- src/test/java/com/smoothstack/BatchMicroservice/generator/GeneratorAndCacheTest.java
+++ src/test/java/com/smoothstack/BatchMicroservice/generator/GeneratorAndCacheTest.java
@@ -1,5 +1,6 @@
 package com.smoothstack.BatchMicroservice.generator;
 
+import com.smoothstack.BatchMicroservice.cache.CardCache;
 import com.smoothstack.BatchMicroservice.cache.UserCache;
 import com.smoothstack.BatchMicroservice.model.User;
 import org.junit.jupiter.api.Test;
@@ -9,9 +10,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
-public class UserGeneratorAndCacheTest {
+public class GeneratorAndCacheTest {
 
     private final UserCache userCache = UserCache.getInstance();
+    private final CardCache cardCache = CardCache.getInstance();
 
     @Test
     public void correctUserGenerationTest(){
@@ -22,5 +24,16 @@ public class UserGeneratorAndCacheTest {
         assertEquals(userOrGenerate.getFirstName(), userCache.getGeneratedUser(0L).getFirstName());
         userCache.getGeneratedUsers().remove(0L);
         assertTrue(userCache.getGeneratedUsers().isEmpty());
+    }
+
+    @Test
+    public void correctCardGenerationTest(){
+        assertTrue(cardCache.getGeneratedCards().isEmpty());
+        cardCache.findOrGenerateCard(0L, 2L);
+        assertEquals(3, cardCache.getGeneratedUserCards(0L).size());
+        cardCache.findOrGenerateCard(0L, 3L);
+        assertEquals(4, cardCache.getGeneratedUserCards(0L).size());
+        cardCache.getGeneratedCards().remove(0L);
+        assertTrue(cardCache.getGeneratedCards().isEmpty());
     }
 }
